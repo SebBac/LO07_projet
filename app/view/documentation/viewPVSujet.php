c3e618b28f430a5590ded0e2af78dbcf1361e08d
SebBac <73548842+SebBac@users.noreply.github.com>
2021-06-18 22:22:09 +0200

    Rédaction PVSujet

--- app/view/documentation/viewPVSujet.php
+++ app/view/documentation/viewPVSujet.php
@@ -17,7 +17,19 @@ require ($root . '/app/view/fragment/fragmentCovidHeader.html');
               <h2 class="panel-title">Notre point de vue global sur ce projet</h2>
           </div>
           <div class="panel-body">
-                Blablabla...
+                Ce projet a été intéressant à réaliser en binôme, notamment sur
+                 l'utilisation de git pour la collaboration, ce qui nous sera
+                 très utile pour de futurs projets. De plus, nous avons pu mobiliser
+                 nos compétences webs acquises lors du semestre. <br>
+                 C'est intéressant d'avoir des questions libres sur des innovations
+                 mais c'est parfois un peu compliqué de trouver une idée, peut-être
+                 proposer une liste d'innovation faisables sur le projet en précisant
+                 que l'on peut nous-même en imaginer d'autres pourrait être intéressant ?
+                 <br>Pour améliorer le site, on pourrait rajouter une nouvelle table
+                 pour les stocks de vaccins possédés par l'état et pas encore
+                 distribués aux centres hospitaliers. Ainsi, il faudrait
+                 d'abord que l'état se fournisse en vaccins avant de pouvoir les
+                 redistribuer aux différents centres.
           </div>
       </div>
       
